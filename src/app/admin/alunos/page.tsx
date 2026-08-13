cd08d86bebad43b8473d1157fece9b14064b383f
deservs <viniciuscapela6014@gmail.com>
2026-08-12 23:38:32 -0300

    foi criado o pop-up de adição de aluno no site na parte de Admin Alunos

--- src/app/admin/alunos/page.tsx
+++ src/app/admin/alunos/page.tsx
@@ -1,28 +1,38 @@
 'use client'
 import styles from "../admin.module.css"
 import { MdDelete, MdEdit, MdAdd } from "react-icons/md"
+import { useState } from "react"
+import Adicionar from "@/app/components/pop-up/Adicionar.Aluno"
 
 function Alunos() {
+  const [showAddAluno, setShowAddAluno] = useState(false);
   return (
     <section>
       <table className={styles.Tabela}>
         <thead>
           <tr>
-          <th className={styles.TabelaNome}>Nome</th>
-          <th className={styles.TabelaCargo}>Cargo</th>
-          <th colSpan={2} className={styles.TabelaAcoes}>Ações</th>
-        </tr>
+            <th className={styles.TabelaNome}>Nome</th>
+            <th className={styles.TabelaCargo}>Cargo</th>
+            <th colSpan={2} className={styles.TabelaAcoes}>Ações</th>
+          </tr>
         </thead>
         <tbody>
           <tr>
-          <td>Gabriel</td>
-          <td className={styles.Cargo}>Comum</td>
-          <td><MdDelete className={styles.TabelaIcon}/></td>
-          <td><MdEdit className={styles.TabelaIcon}/></td>
-        </tr>
+            <td>Gabriel</td>
+            <td className={styles.Cargo}>Comum</td>
+            <td><MdDelete className={styles.TabelaIcon} /></td>
+            <td><MdEdit className={styles.TabelaIcon} /></td>
+          </tr>
         </tbody>
       </table>
-      <button className={styles.addMore}><MdAdd/></button>
+      <button
+        aria-label="Adicionar Aluno"
+        className={styles.addMore}
+        onClick={() => setShowAddAluno(true)}
+      >
+        <MdAdd />
+      </button>
+      {showAddAluno && <Adicionar showAddAluno={showAddAluno} closeShowAddAluno={() => setShowAddAluno(false)} />}
     </section>
   )
 }
---
+++ src/app/components/pop-up/Adicionar.Aluno.tsx
@@ -0,0 +1,53 @@
+'use client'
+import styles from './AddAluno.module.css'
+import { MdClose } from "react-icons/md"
+
+interface PopUpProps {
+    showAddAluno: boolean;
+    closeShowAddAluno: () => void;
+}
+function Adicionar({ showAddAluno, closeShowAddAluno }: PopUpProps) {
+    if (!showAddAluno) {
+        return null;
+    }
+    return (
+        <section>
+            <div className={styles.AddPopUp}>
+                <div className={styles.AddPopUpContent}>
+                    <div className={styles.PopUpHeader}>
+                        <button className={styles.modalClose} onClick={closeShowAddAluno}>
+                            <MdClose />
+                        </button>
+                        <h1>Adicionar Novo Aluno</h1>
+                    </div>
+                    <div className={styles.PopUpNome}>
+                        <label htmlFor="nome">Nome Completo</label>
+                        <input type="text" id="nome" placeholder="Ex: João Silva" />
+                    </div>
+                    <div className={styles.PopUpAdmin}>
+                        <label htmlFor="admin">Acesso Administrativo?</label>
+                        <label htmlFor="admin" className={styles.switch}>
+                            <input type="checkbox" id="admin" />
+                            <span className={styles.slider}></span>
+                        </label>
+                    </div>
+                    <div className={styles.PopUpEquipe}>
+                        <label htmlFor="equipe">Equipe</label>
+                        <select name="equipe" id="equipe">
+                            <option value="">Equipe Vermelha</option>
+                            <option value="">Equipe Azul</option>
+                            <option value="">Equipe Amarela</option>
+                            <option value="">Equipe Verde</option>
+                        </select>
+                    </div>
+                    <div className={styles.PopUpButtons}>
+                        <button>Salvar Aluno</button>
+                        <button onClick={closeShowAddAluno}>Cancelar</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Adicionar
