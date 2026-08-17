99581f19cb3f1d4751f0d45aea252a33fb1027bf
gabriexlss <gabrielsasukeclash@gmail.com>
2026-08-17 09:06:23 -0300

    feat: endpoint para delete de post concluida

--- src/app/api/cronograma/[id]/route.ts
+++ src/app/api/cronograma/[id]/route.ts
@@ -38,6 +38,7 @@ export async function DELETE(req: NextRequest, { params }: RouteParams) {
     }
 }
 
+//  rota pra marcar que o cronograma teve imprevisto
 export async function PATCH(req: NextRequest, { params }: RouteParams) {
     const res = NextResponse  
     try{
@@ -56,15 +57,15 @@ export async function PATCH(req: NextRequest, { params }: RouteParams) {
         }
 
         // realiza a operação
-        const query = "DELETE FROM cronogramas WHERE id = $1"
+        const query = "UPDATE cronogramas SET imprevisto = true WHERE id = $1"
         await pool.query(query,[id])
 
         // revalida cache e manda embora
         revalidatePath('/admin/cronogramas')
         revalidatePath('/cronograma')  
-        return res.json({msg: "Cronograma Deletado com Sucesso."},{status:200})
+        return res.json({msg: "Imprevisto no Cronograma Registrado."},{status:200})
     }catch(erro){
-        console.error("erro no endpoint de deletar cronograma, erro: ", erro)
+        console.error("erro no endpoint de registrar imprevisto, erro: ", erro)
         return res.json({msg: "Erro Interno do Servidor"},{status:500})
     }
 }
